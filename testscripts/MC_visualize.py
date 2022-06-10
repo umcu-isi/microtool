155a0800691970db94e591fd313a5c124c10574a
fwhotto <34309779+fwhotto@users.noreply.github.com>
2022-06-10 12:06:17 +0200

    Seaborn bivariate plot

--- testscripts/MC_visualize.py
+++ testscripts/MC_visualize.py
@@ -1,11 +1,18 @@
 import pathlib
 
 from matplotlib import pyplot as plt
+import seaborn as sns
 
 from microtool.utils import get_df_from_pickle
 
 resultdir = pathlib.Path("MC_results")
-filename = "TPD_dmipy_large.pkl"
+# insrt filename here
+filename = "TPD_relaxation.pkl"
 better_df = get_df_from_pickle(resultdir/filename)
+
 better_df.hist()
+
+plt.figure()
+sns.histplot(better_df, x="C1Stick_1_mu_0" , y = "C1Stick_1_mu_1")
+plt.title(r"$N_{sim} = 10000$")
 plt.show()
