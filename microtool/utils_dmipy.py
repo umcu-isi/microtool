9ea4a0958f7b48818c0b8cae6e489e2ce6ef8d8f
Frank Otto <F.W.H.Otto-2@umcutrecht.nl>
2022-06-23 11:12:50 +0200

    Multi shell sampling
    Implemented multishell sampling

--- microtool/utils_dmipy.py
+++ microtool/utils_dmipy.py
@@ -1,20 +1,79 @@
+import itertools
+import math
+from typing import List
+
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.spatial import distance
 from scipy.optimize import minimize, NonlinearConstraint
+from itertools import combinations
+from numba import njit
 
 
-def main():
-    """ Demonstrating the use of these functions"""
+def test_uniform():
     # Uniform sphere sampling
-    angle_samples = sample_sphere()
-    vector_samples = angles_to_eigenvectors(angle_samples)
+    vector_samples = sample_sphere_vectors()
     _plot_vectors(vector_samples, "Using sample_sphere")
 
     # Optimizing the coulomb potential constraining the points to the sphere
     samples = sample_uniform()
     _plot_vectors(samples, "Electrostatic optimization")
     plt.show()
+
+
+def main():
+    """ Demonstrating the use of these functions"""
+    result = sample_q_shells([9, 9, 9])
+    _plot_shells(result)
+    plt.show()
+
+
+def sample_q_shells(n_shells: List[int]) -> List[np.ndarray]:
+    vec_init = list(map(sample_sphere_vectors, n_shells))
+    x0 = np.concatenate(list(map(np.ravel, vec_init)))
+
+    result = minimize(cost_scipy, x0, args=(n_shells, 0.5), constraints=get_constraints(x0))
+    return repack_shells(result['x'], n_shells)
+
+
+def cost_scipy(x: np.ndarray, n_shells: List[int], alpha: float = 0.5) -> float:
+    # folding the parameter vector
+    vectors = repack_shells(x, n_shells)
+    return alpha * cost_inter_shell(vectors) + (1.0 - alpha) * cost_intra_shell(vectors)
+
+
+def repack_shells(x, n_shells) -> List[np.ndarray]:
+    vectors = []
+    idx = 0
+    for n_q in n_shells:
+        stride = n_q * 3
+        vectors.append(x[idx:(idx + stride)].reshape(n_q, 3))
+        idx += stride
+
+    return vectors
+
+
+def cost_intra_shell(vectors: List[np.ndarray]) -> float:
+    # total number of samples
+    n_total = float(sum(list(map(len, vectors))))  # float type setting
+
+    # go over the unique pair combinations of shells s!=t
+    cost = 0.
+    for vec_shell_s, vec_shell_t in combinations(vectors, 2):
+        for i in range(vec_shell_s.shape[0]):
+            for j in range(vec_shell_t.shape[0]):
+                cost += total_potential(np.vstack((vec_shell_s[i, :], vec_shell_t[j, :])))
+    return cost * (1. / (n_total ** 2))
+
+
+def cost_inter_shell(vectors: List[np.ndarray]) -> float:
+    # Get electrostatic energy for individual shells
+    n_q = len(vectors)  # number of shells
+    cost = 0.0
+    for i in range(n_q):
+        # scale the cost per shell by the number of samples on the shell
+        cost += (1.0 / (float(vectors[i].shape[0]) ** 2)) * total_potential(vectors[i])
+    return cost
 
 
 def sample_uniform(ns: int = 100) -> np.ndarray:
@@ -24,11 +83,14 @@ def sample_uniform(ns: int = 100) -> np.ndarray:
     :param ns: Number of samples on the sphere
     :return: the unit vectors uniform on sphere in shape (ns,3)
     """
-    vec_init = angles_to_eigenvectors(sample_sphere(ns))
+    vec_init = sample_sphere_vectors(ns)
     x0 = vec_init.flatten()
-    constraints = NonlinearConstraint(get_norms, make_bounds(x0), make_bounds(x0))
-    result = minimize(cost_fun, x0, constraints=constraints)
+    result = minimize(cost_fun, x0, constraints=get_constraints(x0))
     return result['x'].reshape((-1, 3))
+
+
+def get_constraints(x: np.ndarray) -> NonlinearConstraint:
+    return NonlinearConstraint(get_norms, make_bounds(x), make_bounds(x))
 
 
 def make_bounds(x):
@@ -53,16 +115,41 @@ def cost_fun(x):
     :return: scalar representing the total energy of the system
     """
     vecs = np.reshape(x, (-1, 3))
-    return np.sum(potential(vecs))
+    return total_potential(vecs)
 
 
-def potential(vectors: np.ndarray) -> np.ndarray:
+def total_potential(vectors: np.ndarray) -> np.ndarray:
     """
     Computes the potential energy generated between pairs of points
     :param vectors: an array of shape (n_vec,3)
     :return: pairwise potential array
     """
-    return 1 / distance.pdist(vectors)
+    return np.sum(1 / distance.pdist(vectors))
+
+
+def make_sphere(r):
+    pi = np.pi
+    cos = np.cos
+    sin = np.sin
+    phi, theta = np.mgrid[0.0:pi:100j, 0.0:2.0 * pi:100j]
+    x = r * sin(phi) * cos(theta)
+    y = r * sin(phi) * sin(theta)
+    z = r * cos(phi)
+    return x, y, z
+
+
+def _plot_shells(shells: List[np.ndarray], title: str = None) -> None:
+    plt.figure()
+    ax = plt.axes(projection='3d')
+
+    color = iter(plt.cm.rainbow(np.linspace(0, 1, len(shells))))
+    for i, vecs in enumerate(shells):
+        c = next(color)
+        scale_factor = float(i + 1)
+        ax.scatter3D(scale_factor * vecs[:, 0], scale_factor * vecs[:, 1], scale_factor * vecs[:, 2], color=c)
+        ax.plot_surface(*make_sphere(i + 1), alpha=0.2, color='gray')
+
+    plt.tight_layout()
 
 
 def _plot_vectors(vectors: np.ndarray, title: str) -> None:
@@ -122,7 +209,11 @@ def angles_to_eigenvectors(angles: np.ndarray) -> np.ndarray:
     return np.stack([x, y, z], axis=-1)
 
 
-def sample_sphere(ns: int = 100) -> np.ndarray:
+def sample_sphere_vectors(ns: int = 100) -> np.ndarray:
+    return angles_to_eigenvectors(sample_sphere_angles(ns))
+
+
+def sample_sphere_angles(ns: int = 100) -> np.ndarray:
     """ Makes uniform RANDOM samples on the sphere in terms of spherical angles
 
     :param ns: Number of samples you wish to take on the sphere
