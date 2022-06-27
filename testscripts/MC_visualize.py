503ea953791a85e464e7ce38d7dc0aa9ac86d6f0
Frank Otto <F.W.H.Otto-2@umcutrecht.nl>
2022-06-27 16:13:35 +0200

    Plot scaled parameter difference

--- testscripts/MC_visualize.py
+++ testscripts/MC_visualize.py
@@ -5,17 +5,25 @@ import math
 from microtool.utils_IO import get_df_from_pickle, get_pickle
 
 resultdir = pathlib.Path("MC_results")
-filename = "TPD_alexander2008_nsim10_npulse2_noise0.02.pkl"
+filename = "debug.pkl"
 df = get_df_from_pickle(resultdir / filename)
 gt = get_pickle(resultdir / "alexander2008_ground_truth.pkl")
 
 n_rows = math.ceil(df.shape[1] / 3)
 
+
 for i, parameter in enumerate(df.keys()):
     ax = plt.subplot(n_rows, 3, i + 1)
+
+    gt_parameter = gt[parameter]
+    scale = gt_parameter.scale
+    value = gt_parameter.value / gt_parameter.scale
+
     # making the histogram
-    ax.hist(df[parameter], bins=20)
-    ax.vlines(gt[parameter], 0, 1, transform=ax.get_xaxis_transform(), colors="red", label="Ground Truth")
+    ax.hist(df[parameter] / scale - value, bins=100)
+    ax.set_xlabel(r"$\Delta$")
+    # plotting ground truth as vertical lines
+    ax.vlines(0, 0, 1, transform=ax.get_xaxis_transform(), colors="red", label="Ground Truth")
     ax.set_title(parameter)
 
 plt.tight_layout()
