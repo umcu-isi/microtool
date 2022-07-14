a65f450d2093a257ba9b67d62c75f980c75b7f20
Frank Otto <F.W.H.Otto-2@umcutrecht.nl>
2022-07-14 15:44:42 +0200

    updated dmipy_tests

--- tests/test_dmipy.py
+++ tests/test_dmipy.py
@@ -13,20 +13,21 @@ as simulating signal using the same pure dmipy model and scheme.
 
 """
 import numpy as np
-import pytest
 from dmipy.core.modeling_framework import MultiCompartmentModel
-
 from dmipy.data import saved_acquisition_schemes
 from dmipy.signal_models import cylinder_models
-from dmipy.core.acquisition_scheme import DmipyAcquisitionScheme
 
 from microtool.dmipy import DmipyAcquisitionSchemeWrapper, DmipyTissueModel, convert_acquisition_scheme
 
 
-class TestSchemeWrapping:
+def test_scheme_wrapping():
     """
-    Here we collect all tests concerning DmipyAcquisitionSchemeWrapper and convert_acquisition_scheme
+    Since some of the DmipyAcquisitionScheme attributes are strange objects we test here for all float,int or
+    numpy array attributes to check if they are approximately equal.
+
+    Failure of this test means there is something wrong with microtool.dmipy.DmipyAcquisitionSchemeWrapper
     """
+    # going over all attributes that are python native or numpy native and asserting equality
     # Standard acquisition scheme from dmipy.data
     naked_scheme = saved_acquisition_schemes.wu_minn_hcp_acquisition_scheme()
     # Micrtool scheme wrapper
@@ -34,45 +35,34 @@ class TestSchemeWrapping:
     # Microtool scheme converter
     converted_wrapped_scheme = convert_acquisition_scheme(wrapped_scheme)
 
-    def test_instance(self):
-        """
-        converting the acquisition scheme should yield a DmipyAcquisitionScheme
-        """
-        assert isinstance(self.converted_wrapped_scheme, DmipyAcquisitionScheme)
-
-    def test_attributes(self):
-        """
-        Since some of the DmipyAcquisitionScheme attributes are strange objects we test here for all float,int or
-        numpy array attributes to check if they are approximately equal.
-
-        Failure of this test means there is something wrong with microtool.dmipy.DmipyAcquisitionSchemeWrapper
-        """
-        # going over all attributes that are python native or numpy native and asserting equality
-
-        naked_attributes = vars(self.naked_scheme)
-        converted_attributes = vars(self.converted_wrapped_scheme)
-        for attribute, value in naked_attributes.items():
-            report = f"The attribute {attribute} triggered an assertion, i.e., they are not equal for both " \
-                     f"scheme types "
-            if isinstance(value, np.ndarray):
-                np.testing.assert_allclose(value, converted_attributes[attribute], rtol=0, atol=1e-5), report
-            elif isinstance(value, (float, int)):
-                assert value == converted_attributes[attribute], report
+    naked_attributes = vars(naked_scheme)
+    converted_attributes = vars(converted_wrapped_scheme)
+    for attribute, value in naked_attributes.items():
+        report = f"The attribute {attribute} triggered an assertion, i.e., they are not equal for both " \
+                 f"scheme types "
+        if isinstance(value, np.ndarray):
+            np.testing.assert_allclose(value, converted_attributes[attribute], rtol=0, atol=1e-5), report
+        elif isinstance(value, (float, int)):
+            assert value == converted_attributes[attribute], report
 
 
 class TestTissueModelWrapping:
     """
-    Here we collected all test concerning DmipyTissueModel.__call__ method
+    Here we collected all test concerning DmipyTissueModel class
     """
-    # The pure dmipy scheme
+    # The pure dmipy stuff
     scheme = saved_acquisition_schemes.wu_minn_hcp_acquisition_scheme()
 
-    # simplest tissuemodel available in dmipy
     mu = (np.pi / 2., np.pi / 2.)  # in radians
     lambda_par = 1.7e-9  # in m^2/s
     stick = cylinder_models.C1Stick(mu=mu, lambda_par=lambda_par)
     stick_model = MultiCompartmentModel(models=[stick])
+
     parameters = {'C1Stick_1_mu': mu, 'C1Stick_1_lambda_par': lambda_par}
+    signal = stick_model.simulate_signal(scheme, parameters)
+
+    # common wrapped parameters
+    stick_model_wrapped = DmipyTissueModel(stick_model)
 
     def test_simulate_signal(self):
         """
@@ -81,10 +71,19 @@ class TestTissueModelWrapping:
 
         In otherwords if this test is passed then DmipyTissueModel.__call__ is correct
         """
-        stick_model_wrapped = DmipyTissueModel(self.stick_model)
-        wrapped_signal = stick_model_wrapped(self.scheme)
-        naked_signal = self.stick_model.simulate_signal(self.scheme, self.parameters)
-        np.testing.assert_allclose(wrapped_signal, naked_signal)
+
+        wrapped_signal = self.stick_model_wrapped(self.scheme)
+
+        np.testing.assert_allclose(wrapped_signal, self.signal)
+
+    def test_fit(self):
+        """
+        Testing if dmipy fits properly. Fails because of orientation degeneracy ???
+        """
+        fitted_model = self.stick_model.fit(self.scheme, self.signal)
+        for parameter, value in fitted_model.fitted_parameters.items():
+            expected = np.array(self.parameters[parameter])
+            np.testing.assert_allclose(np.array(value).flatten(), expected)
 
 
 def test_model_scheme_integration():
