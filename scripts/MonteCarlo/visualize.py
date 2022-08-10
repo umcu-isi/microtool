79a32fd1f34f49e9c8b7ae87726099886ee3861f
Frank Otto <F.W.H.Otto-2@umcutrecht.nl>
2022-08-10 14:31:58 +0200

    Generated results for different shell distributions
    Still changing the visualisation workflow a little bit.

--- scripts/MonteCarlo/visualize.py
+++ scripts/MonteCarlo/visualize.py
@@ -7,30 +7,38 @@ from matplotlib import pyplot as plt
 from microtool.utils.IO import get_df_from_pickle, get_pickle
 
 resultdir = pathlib.Path(__file__).parent / 'results'
-filename = "alexander_nofixed_n_sim_1000_noise_0.02.pkl"
+filename = "alexander_shells_n_sim_2_noise_0.02.pkl"
 df = get_df_from_pickle(resultdir / filename)
 gt = get_pickle(resultdir / "alexander2008_ground_truth.pkl")
 
-n_rows = math.ceil(df.shape[1] / 3) + 1
+def main():
+    pass
 
-print(df.describe())
-for i, parameter in enumerate(df.keys()):
-    ax = plt.subplot(n_rows, 3, i + 1)
+def visualize_pickle(filename: str) -> None:
+    n_rows = math.ceil(df.shape[1] / 3) + 1
 
-    gt_parameter = gt[parameter]
-    scale = gt_parameter.scale
-    value = gt_parameter.value / gt_parameter.scale
+    print(df.describe())
+    for i, parameter in enumerate(df.keys()):
+        ax = plt.subplot(n_rows, 3, i + 1)
 
-    # making the histogram
-    ax.hist(df[parameter] / scale - value, bins='scott')
-    ax.set_xlabel(r"$\Delta$")
-    # plotting ground truth as vertical lines
-    ax.vlines(0, 0, 1, transform=ax.get_xaxis_transform(), colors="red", label="Ground Truth")
-    ax.set_title(parameter)
+        gt_parameter = gt[parameter]
+        scale = gt_parameter.scale
+        value = gt_parameter.value / gt_parameter.scale
 
-# Adding a table for the ground truth values
-gt_dict = gt.parameters
-pprint.pprint(gt_dict)
+        # making the histogram
+        ax.hist(df[parameter] / scale - value, bins='scott')
+        ax.set_xlabel(r"$\Delta$")
+        # plotting ground truth as vertical lines
+        ax.vlines(0, 0, 1, transform=ax.get_xaxis_transform(), colors="red", label="Ground Truth")
+        ax.set_title(parameter)
 
-plt.tight_layout()
+    # Adding a table for the ground truth values
+    gt_dict = gt.parameters
+    pprint.pprint(gt_dict)
+    plt.tight_layout()
+
+
 plt.show()
+
+if __name__ == "__main__":
+    main()
