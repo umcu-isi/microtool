6b26352ff7c3d470f54bd40543a00748f89d0134
Frank Otto <F.W.H.Otto-2@umcutrecht.nl>
2022-08-18 12:39:26 +0200

    Implemented AMICO acquisition scheme
    Problem with the scheme optimization persist, as such debugging is necessary.

--- scripts/VERDICT.py
+++ scripts/VERDICT.py
@@ -1,3 +1,8 @@
+"""
+Script demonstrating the optimization of VERDICT tissue model. For now we are using an initial scheme from
+DOI:10.1002/nbm.4019
+"""
+import matplotlib.pyplot as plt
 import numpy as np
 from dmipy.core.acquisition_scheme import acquisition_scheme_from_bvalues
 from dmipy.core.modeling_framework import MultiCompartmentModel
@@ -9,62 +14,72 @@ from microtool.optimize import optimize_scheme
 
 def main():
     # ------------------- Tissue model
-
     # The three models we will be using to model intra cellular, extra cellular and Vascular signal respectively
     # Initialize diameter of the cell i.e. sphere in the middle of optimization domain
     sphere = sphere_models.S4SphereGaussianPhaseApproximation(diffusion_constant=0.9e-9, diameter=10e-6)
     ball = gaussian_models.G1Ball(lambda_iso=0.9e-9)
     stick = cylinder_models.C1Stick(mu=[0, 0], lambda_par=6.5e-9)
 
-    verdict_mod = MultiCompartmentModel(models=[sphere, ball, stick])
+    verdict_model = MultiCompartmentModel(models=[sphere, ball, stick])
 
-    print('The multicompartment model has the following parameters', verdict_mod.parameter_names)
+    print('The multicompartment model has the following parameters', verdict_model.parameter_names)
 
     # We fix the extra cellular diffusivuty in accordance with panagiotakis black magic number from 2014
-    verdict_mod.set_fixed_parameter('G1Ball_1_lambda_iso', 0.9e-9)
+    verdict_model.set_fixed_parameter('G1Ball_1_lambda_iso', 0.9e-9)
     # We set the optimization such that the pseudo diffusioin coefficient is larger than 3.05 um^2/ms
-    verdict_mod.set_parameter_optimization_bounds('C1Stick_1_lambda_par', [3.05e-9, 10e-9])
+    verdict_model.set_parameter_optimization_bounds('C1Stick_1_lambda_par', [3.05e-9, 10e-9])
 
-    verdict_mod = DmipyTissueModel(verdict_mod, np.array([.3, .3, .4]))
-    print("The verdict tissue model:", verdict_mod)
+    verdict_model = DmipyTissueModel(verdict_model, np.array([.3, .3, .4]))
+    print("The verdict tissue model:", verdict_model)
+
     # ------------------- Acquisition Scheme
-
-    # setting some random b0 measurements because alexander does not specify.
-    n_b0 = 18
-    b0 = np.zeros(n_b0)
-    zero_directions = sample_uniform(n_b0)
-    zero_delta = np.repeat(0.007, n_b0)
-    zero_Delta = np.repeat(0.012, n_b0)
-
     b_values = np.array([90, 500, 1500, 2000, 3000]) * 1e6
-    T_E = np.array([50, 65, 90, 67, 90]) * 1e-3
-
+    TE = np.array([50, 65, 90, 71, 80]) * 1e-3
     gradient_directions = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+    Delta = np.array([23.8, 31.3, 43.8, 34.3, 38.8]) * 1e-3
+    delta = np.array([3.9, 11.4, 23.9, 14.4, 18.9]) * 1e-3
 
     # Extending the arrays so dmipy understands what we mean
     n_measurements = len(b_values)
     n_directions = len(gradient_directions)
 
-    Delta = np.array([23.8, 31.3, 43.4, 32.3, 43.8]) * 1e-3
-    delta = np.array([12.2, 12., 24.7, 13.2, 24.7]) * 1e-3
+    # Executing a b0 measurement for every delta,Delta and TE combination (i.e. every b-value) so we can compensate
+    # for T1 and T2 dependence.
+    n_b0 = len(delta)
+    b0 = np.zeros(n_b0)
+    zero_directions = sample_uniform(n_b0)
+    zero_delta = delta
+    zero_Delta = Delta
+    zero_TE = TE
 
     # Converting to dmipy compatible ,i.e., prepending b0 values and repeating for every direction
     b_values = convert_to_dmipy_acquisition_parameter(b0, b_values, n_directions)
     delta = convert_to_dmipy_acquisition_parameter(zero_delta, delta, n_directions)
     Delta = convert_to_dmipy_acquisition_parameter(zero_Delta, Delta, n_directions)
+    TE = convert_to_dmipy_acquisition_parameter(zero_TE, TE, n_directions)
 
     # Extending directions for every measurement
     gradient_directions = np.tile(sample_uniform(n_directions), (n_measurements, 1))
+
     # Prepending b0 measurements
     gradient_directions = np.concatenate([zero_directions, gradient_directions], axis=0)
-    scheme = acquisition_scheme_from_bvalues(b_values, gradient_directions, delta, Delta)
+    scheme = acquisition_scheme_from_bvalues(b_values, gradient_directions, delta, Delta, TE=TE)
     scheme.print_acquisition_info
     scheme = DmipyAcquisitionSchemeWrapper(scheme)
 
     print(scheme)
 
+    # Signal plotting
+    plt.figure('Signal plot')
+    plt.plot(verdict_model(scheme), '.')
+    plt.xlabel('Measurement')
+    plt.xticks(range(scheme.pulse_count), np.array(range(scheme.pulse_count)) + 1)
+    plt.ylabel(r'$S/S_0$')
+    plt.show()
+
     # -------------- Optimization
-    best_scheme, _ = optimize_scheme(scheme, verdict_mod, 0.02)
+    best_scheme, _ = optimize_scheme(scheme, verdict_model, 0.02)
 
 
 def convert_to_dmipy_acquisition_parameter(par_b0, par_measurements, n_directions):
