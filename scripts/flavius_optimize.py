8ee8bd2997c6c3446de4d15062111ce49379d5e8
Frank Otto <F.W.H.Otto-2@umcutrecht.nl>
2022-09-15 15:54:29 +0200

    Changed to a converging optimizer.

--- scripts/flavius_optimize.py
+++ scripts/flavius_optimize.py
@@ -1,14 +1,15 @@
 """
 Here we optimize the scheme according to flavius' specifications:
 """
+from copy import deepcopy
+
 import matplotlib.pyplot as plt
 import numpy as np
 
+from microtool.acquisition_scheme import FlaviusAcquisitionScheme
 from microtool.optimize import optimize_scheme
-from microtool.tissue_model_flavius import FlaviusSignalModel
-from microtool.acquisition_scheme_flavius import FlaviusAcquisitionScheme
+from microtool.tissue_model import FlaviusSignalModel
 from microtool.utils.solve_echo_time import minimal_echo_time
-from copy import deepcopy
 
 # dont print so many digits
 np.set_printoptions(formatter={'float': '{: .2e}'.format})
@@ -41,7 +42,12 @@ TE = np.array(
 
 scheme = FlaviusAcquisitionScheme(b, TE, **scan_parameters)
 scheme_copy = deepcopy(scheme)
-scheme_optimized, _ = optimize_scheme(scheme, signal_model, noise_variance=0.02, repeat=1)
+
+# ----------- Optimization
+optimizer_options = {"maxiter": 1000, 'disp': True}
+
+scheme_optimized, _ = optimize_scheme(scheme, signal_model,
+                                      noise_variance=0.02, repeat=1, method='SLSQP', options=optimizer_options)
 
 print('Optimized Scheme: \n', scheme_optimized)
 
@@ -69,7 +75,7 @@ t_rise = scheme_optimized['RiseTime'].values
 t_half = scheme_optimized['HalfReadTime'].values
 
 # Changing to ms/mm^2 for computing minimal echo time.
-b_plot = np.linspace(1, 30e3, num=500) * 1e3
+b_plot = np.linspace(0.1, 15e3, num=500) * 1e3
 plt.plot(minimal_echo_time(b_plot, t90, t180, t_half, G_max, t_rise), b_plot * 1e-3, label="Analytic lowerbound")
 plt.xlabel("TE")
 plt.ylabel('b')
