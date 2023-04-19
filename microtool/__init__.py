e6d3e7b62b11fede10e6c75e49718aeccc758754
fwhotto <fwhotto@gmail.com>
2023-04-19 13:57:29 +0200

    Fix __init__ file
    __init__ should be empty to find all submodules of microtool.

--- microtool/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import acquisition_scheme
-from . import optimize
-from . import tissue_model
-
-# These modules are part of the public interface.
-__all__ = ['acquisition_scheme', 'optimize', 'tissue_model']
