d30f8eedf5d42bde7b73e81d832c4763a924b00d
fwhotto <fwhotto@gmail.com>
2023-09-14 15:46:42 +0200

    Made alexanders optimal scheme

--- microtool/utils/saved_schemes.py
+++ microtool/utils/saved_schemes.py
@@ -2,8 +2,39 @@ import numpy as np
 
 from microtool.acquisition_scheme import InversionRecoveryAcquisitionScheme, DiffusionAcquisitionScheme
 from microtool.constants import PULSE_TIMING_LB, PULSE_TIMING_UB
-from microtool.gradient_sampling import sample_uniform
+from microtool.gradient_sampling import sample_uniform_half_sphere
 from microtool.scanner_parameters import default_scanner
+
+
+def make_repeated_measurements(val_list, N_rep):
+    return np.concatenate([np.repeat(val, N_rep) for val in val_list])
+
+
+def alexander_optimal_perturbed() -> DiffusionAcquisitionScheme:
+    N = 30  # Number of measurement directions
+    M = 4  # "measurements" i.e. unique acquisition parameter combinations
+    N_pulses = N * M
+
+    G_max = 0.2  # T m^-1
+    default_scanner.G_max = G_max
+
+    gradient_magnitudes = make_repeated_measurements([0.2, 0.2, 0.121, 0.2], N)
+    gradient_directions = np.tile(sample_uniform_half_sphere(N), (M, 1))
+    pulse_intervals = make_repeated_measurements([0.025, 0.026, 0.029, 0.013], N)
+    pulse_widths = make_repeated_measurements([0.02, 0.018, 0.016, 0.008], N)
+    scheme = DiffusionAcquisitionScheme(gradient_magnitudes, gradient_directions, pulse_widths, pulse_intervals,
+                                        scan_parameters=default_scanner)
+
+    # fix echo time to max values
+    scheme["EchoTime"].values = np.repeat(PULSE_TIMING_UB, N_pulses)
+    scheme["EchoTime"].set_fixed_mask(np.ones(N_pulses, dtype=bool))
+
+    # mark the repeated parameters
+    repeated_parameters = ['DiffusionPulseMagnitude', 'DiffusionPulseWidth', 'DiffusionPulseInterval']
+    for parameter in repeated_parameters:
+        scheme[parameter].set_repetition_period(N)
+
+    return scheme
 
 
 def alexander_initial_random() -> DiffusionAcquisitionScheme:
@@ -13,7 +44,7 @@ def alexander_initial_random() -> DiffusionAcquisitionScheme:
     G_max = 0.2  # T m^-1
     default_scanner.G_max = G_max
     gradient_magnitudes = np.random.uniform(0.0, G_max, N_pulses)
-    gradient_directions = np.tile(sample_uniform(N), (M, 1))
+    gradient_directions = np.tile(sample_uniform_half_sphere(N), (M, 1))
 
     pulse_intervals = np.linspace(PULSE_TIMING_LB, 0.02, N_pulses) + 0.01
     pulse_widths = np.linspace(PULSE_TIMING_LB, 0.01, N_pulses)
--- scripts/demo/alexander_2008.py
+++ scripts/demo/alexander_2008.py
@@ -4,19 +4,20 @@ from microtool.optimize import optimize_scheme
 from microtool.utils.IO import save_pickle
 from microtool.utils.plotting import LossInspector
 from microtool.utils.saved_models import cylinder_zeppelin
-from microtool.utils.saved_schemes import alexander_initial_random
+from microtool.utils.saved_schemes import alexander_optimal_perturbed
 
 
 def main():
-    initial_scheme = alexander_initial_random()
+    initial_scheme = alexander_optimal_perturbed()
     model = cylinder_zeppelin()
+    print(model)
     print(initial_scheme)
     loss_inspector = LossInspector(initial_scheme, model, noise_var=.02)
     loss_inspector.plot([{"DiffusionPulseWidth": 1}])
     plt.savefig("loss_landscape.png")
 
-    optimal_scheme, _ = optimize_scheme(initial_scheme, model, noise_variance=.02,
-                                        solver_options={"strategy": "rand1exp"})
+    optimal_scheme, _ = optimize_scheme(initial_scheme, model, noise_variance=.02, method="trust-constr",
+                                        solver_options={"disp": True})
 
     print(optimal_scheme)
     save_pickle(optimal_scheme, "alexander_optimized.pkl")
